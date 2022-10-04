4d828a0442ba36b0e8ab8a261680a44930ca70cf
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-04 16:07:52 -0400

    workinng on ouyput function

--- Scripts/index.js
+++ Scripts/index.js
@@ -6,22 +6,26 @@ const interestRateField = document.getElementById("interestRateField");
 const loanLengthField = document.getElementById("loanLengthField");
 const resultMonthlyPayment = document.getElementById("resultMonthlyPayment");
 const resultTotalInterestPaid = document.getElementById("resultTotalInterestPaid");
+const compoundRate = document.getElementById("compoundField");
+const mortgageOutput = document.getElementById("mortgageField");
 const num1 = 1;
 
 let principal = Number(principalField.value);
 let interestRate = Number(interestRateField.value);
 let loanLength = Number(loanLengthField.value);
 let percentInterest = interestRate;
+let compound = Number(compoundRate.value);
 
 function calculateMortgage(principal, percentInterest, loanLength) {
-    let monthly = (principal * percentInterest) / num1 - (num1  + percentInterest)**loanLength;
-    return monthly;
+    let monthly = Number(principal);
+    mortgageOutput.innerHTML = `Your monthly payment is ${monthly}`;
 }
 
 console.log(calculateMortgage(principal, percentInterest,loanLength));
 
 function calulateTotalInterest(params) {
     let interest = principal * percentInterest * loanLength;
-    return 
+    
 }
+
 console.log(principal * percentInterest * loanLength);
