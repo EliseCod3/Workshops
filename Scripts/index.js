35a87abc573544eb7b44ec47426a6387ec915757
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-03 22:23:30 -0400

    fixed structure and working on function

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,32 +3,26 @@
 
 
 
+//query html elements on the page and put them into variables
+const principalField = document.getElementById("principalField");
+const interestRateField = document.getElementById("interestRateField");
+const loanLengthField = document.getElementById("loanLengthField");
+const resultMonthlyPayment = document.getElementById("resultMonthlyPayment");
+const resultTotalInterestPaid = document.getElementById("resultTotalInterestPaid");
+const num1 = 1;
 
-function calculateMortgage() {
-    //query html elements on the page and put them into variables
-    const principalField = document.getElementById("principalField");
-    const interestRateField = document.getElementById("interestRateField");
-    const loanLengthField = document.getElementById("loanLengthField");
-    const resultMonthlyPayment = document.getElementById("resultMonthlyPayment");
-    const resultTotalInterestPaid = document.getElementById("resultTotalInterestPaid");
-    const num1 = 1;
+let principal = Number(principalField.value);
+let interestRate = Number(interestRateField.value);
+let loanLength = Number(loanLengthField.value);
+let percentInterest = interestRate;
 
-    //calculate something
-    let principal = Number(principalField.value);
-    let interestRate = Number(interestRateField.value);
-    let loanLength = Number(loanLengthField.value);
-    let percentInterest = interestRate;
-    let monthly = principal * percentInterest / num1 - (num1  + percentInterest)**loanLength;
-    let interest = principal * percentInterest * loanLength;
-    console.log(principal * percentInterest * loanLength);
-
-    //create your message to display
-    let answerMortgage = `Your expected monthly payment is ${monthly}`;
-    let answerTotalInterest = `Your total interest paid is ${interest}`;
-
-    //display message
-    resultMonthlyPayment.innerHTML = answerMortgage;
-    resultTotalInterestPaid.innerHTML = answerTotalInterest;
+function calculateMortgage(principal, percentInterest, loanLength) {
+    
+    let monthly = (principal * percentInterest) / num1 - (num1  + percentInterest)**loanLength;
+    return monthly;
 }
 
-calculateMortgage()
+console.log(calculateMortgage());
+
+let interest = principal * percentInterest * loanLength;
+console.log(principal * percentInterest * loanLength);
