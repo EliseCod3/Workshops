f008536f5066236a2d751a147d9b44b664525c56
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-10-06 14:05:50 -0400

    fixed mortgage number value

--- Workshop1/Scripts/index.js
+++ Workshop1/Scripts/index.js
@@ -25,14 +25,18 @@ function calculateMortgage() {
 console.log(calculateMortgage());
 
 function calulateCompoundInterest() {
+    const loanLengthField = document.getElementById("loanLengthField");
+    const principalField = document.getElementById("principalField");
     const interestRateField = document.getElementById("interestRateField");
     const compoundRate = document.getElementById("compoundField");
     const mortgageOutput = document.getElementById("mortgageField");
     
+    let loanLength = (loanLengthField.value);
+    let principal = (principalField.value);
     let interestRate = Number(interestRateField.value);
-    let interest = principal * percentInterest * loanLength;
+    let interest = principal * interestRate * loanLength;
     
-    let compound = Number(compoundRate.value);
+    //let compound = Number(compoundRate.value);
 }
 
 console.log(calulateCompoundInterest());
