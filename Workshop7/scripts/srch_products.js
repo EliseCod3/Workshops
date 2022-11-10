de20f3d3125cdbc16e59f1ebd3e1c1bc78852ef1
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-11-10 11:45:29 -0500

    category table loads

--- Workshop7/scripts/srch_products.js
+++ Workshop7/scripts/srch_products.js
@@ -10,6 +10,7 @@ const tableDisplaySection = document.getElementById("tbl-display-section");
 function selectSearchType() {
     let selectedValue = searchTypeDropdown.value;
     
+    
     if(selectedValue == "categories") {
         loadCategoryDropdown();
     }else if (selectedValue == "all") {
@@ -33,6 +34,38 @@ function loadCategoryDropdown() {
             categoryDropdown.appendChild(option);
         }
     })
+}
+
+function loadProductsByCategories() {
+    tableDisplaySection.style.display = "block";
+    
+    const selectedCategory = categoryDropdown.value;
+
+    fetch("http://localhost:8081/api/products")
+    .then((response) => response.json())
+    .then((products) => {
+        products.forEach((product) => {
+            if (product.categoryId == selectedCategory) {
+                tableDisplaySection.style.display = "block";
+                let row = productsTableBody.insertRow(-1);
+                let cell1 = row.insertCell(0);
+                let cell2 = row.insertCell(1);
+                let cell3 = row.insertCell(2);
+                let cell4 = row.insertCell(3);
+                let price = Number(product.unitPrice);
+    
+                cell1.innerText = product.productId;
+                cell2.innerText = product.productName;
+                cell3.innerText = price.toFixed(2);
+    
+                const anchor = document.createElement("a");
+                anchor.innerText = "see details";
+                anchor.href = `./details.html?productId=${product.productId}`;
+                cell4.appendChild(anchor);
+            }
+        });
+    })
+
 }
 
 function loadAllProductsTable() {
@@ -63,4 +96,5 @@ function loadAllProductsTable() {
 
 window.onload = () => {
     searchTypeDropdown.onchange = selectSearchType;
+    categoryDropdown.onchange = loadProductsByCategories;
 }
