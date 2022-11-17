f7722eefb56ff0ff5f98dc17c42e59575c0f6d90
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-11-17 13:28:31 -0500

    tasks load

--- Workshop8/scripts/todos.js
+++ Workshop8/scripts/todos.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const userSelectDropdown = document.querySelector("#user-select-dropdown");
+const tbody = document.querySelector("#todo-table-display tbody");
 
 function loadUsers() {
     fetch("http://localhost:8083/api/users")
@@ -20,7 +21,21 @@ function loadTodos() {
     fetch("http://localhost:8083/api/todos/byuser/" + userId)
     .then(response => response.json())
     .then(user => {
-        
+        tbody.innerHTML = "";
+        for (const userTasks of user) {
+            let row = tbody.insertRow(-1);
+            let cell1 = row.insertCell(0);
+            let cell2 = row.insertCell(1);
+            let cell3 = row.insertCell(2);
+            let cell4 = row.insertCell(3);
+            let cell5 = row.insertCell(4);
+            cell1.innerHTML = userTasks.completed ? "&#10003;": "&#215;";
+            cell2.innerText = userTasks.category;
+            cell3.innerText = userTasks.description;
+            cell4.innerText = userTasks.deadline;
+            cell5.innerText = userTasks.priority;
+            
+        }
     })
 }
 
