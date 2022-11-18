f0c0e1328dd8952c708e4dc54615db4408de5ce8
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-11-18 16:15:31 -0500

    working on mark completed btn

--- Workshop8/scripts/details.js
+++ Workshop8/scripts/details.js
@@ -2,12 +2,105 @@
 
 const $q = (selector) => document.querySelector(selector);
 const usersNameForTask = $q("h1");
-const categoryOutput = $q("h3");
+const categoryOutput = $q("h4");
 const completedOutput = $q("#task-completed-output");
 const deadlineOutput = $q("#task-deadline-output");
-const priorityOutput = $q("task-priority-output");
-const descriptionOutput = $q("task-description-output");
+const priorityOutput = $q("#task-priority-output");
+const descriptionOutput = $q("#task-description-output");
+const backBtn = $q("#back-btn");
+const markCompletedBtn = document.getElementById("mark-completed-btn")
+const unmarkCompletionBtn = document.getElementById("unmark-completion")
+
+function getTaskId() {
+    const urlParams = new URLSearchParams(location.search);
+    return urlParams.get("taskid");
+}
+
+function displayTaskDetails(id) {
+
+    fetch("http://localhost:8083/api/todos/" + id)
+    .then(response => response.json())
+    .then(task => {
+        categoryOutput.innerText = task.category;
+        deadlineOutput.innerText = task.deadline;
+        priorityOutput.innerText = task.priority;
+        completedOutput.innerHTML = task.completed ? "&#10003;": "&#215;";
+        descriptionOutput.innerText = task.description;
+        getUsersName(task.userid);
+        disableMarkCompletedBtn(task.completed.value);
+    })
+
+}
+
+function getUsersName(userId) {
+    fetch("http://localhost:8083/api/users")
+    .then(response => response.json())
+    .then(users => {
+        for (const user of users) {
+            if(user.id == userId) {
+                usersNameForTask.innerText = `${user.name}'s Task `
+            }
+        }
+    })
+}
+
+function markTaskAsCompleted() {
+
+    const taskId = getTaskId();
+
+        const completedTask = {
+            completed: true,
+        }
+    
+        fetch("http://localhost:8083/api/todos/" + taskId, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(completedTask)
+        }).then(response => response.json())
+        .then(task => {
+            console.log(task)
+            completedOutput.innerHTML = task.completed ? "&#10003;": "&#215;";
+        })
+
+}
+
+function unmarkTask() {
+    const taskId = getTaskId();
+
+    const completedTask = {
+        completed: false,
+    }
+    
+        fetch("http://localhost:8083/api/todos/" + taskId, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json"},
+            body: JSON.stringify(completedTask)
+        }).then(response => response.json())
+        .then(task => {
+            console.log(task)
+            completedOutput.innerHTML = task.completed ? "&#10003;": "&#215;";
+
+        })
+
+}
+
+function goBackToTaskList() {
+    window.location = "./todos.html";
+}
+
+function disableMarkCompletedBtn(completion) {
+    if(completion == true) {
+        markCompletedBtn.disabled = true;
+    }else if(completion == false) {
+        markCompletedBtn.disabled = false;
+        unmarkCompletionBtn.disabled = true;
+    }
+}
+
 
 window.onload = () => {
-
+    displayTaskDetails(getTaskId());
+    backBtn.onclick = goBackToTaskList;
+    markCompletedBtn.onclick = markTaskAsCompleted;
+    unmarkCompletionBtn.onclick = unmarkTask;
 }
