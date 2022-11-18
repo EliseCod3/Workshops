336dfcb5f6b292ab188851dd99e88d0195c21c29
elise bell <eliseraynahasawsomesauce@gmail.com>
2022-11-18 12:07:37 -0500

    variables pulled to js

--- Workshop8/scripts/details.js
+++ Workshop8/scripts/details.js
@@ -1,5 +1,13 @@
 "use strict";
 
+const $q = (selector) => document.querySelector(selector);
+const usersNameForTask = $q("h1");
+const categoryOutput = $q("h3");
+const completedOutput = $q("#task-completed-output");
+const deadlineOutput = $q("#task-deadline-output");
+const priorityOutput = $q("task-priority-output");
+const descriptionOutput = $q("task-description-output");
+
 window.onload = () => {
-    
+
 }
